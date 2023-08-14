af5846f731f9cae0129aad0c64ddffe88772e835
deigo-lps <diego.lopes0909@gmail.com>
2023-08-14 18:07:18 -0300

    adiciona active ao link ativo

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,27 @@
 const links = document.querySelectorAll(".page-link");
 const urlParams = new URLSearchParams(window.location.search);
 
-if(!urlParams.get("page")){
-  urlParams.set("page","balance")
-  window.history.pushState({page_id: "balance"},"",`?${urlParams}`)
+const handleCurrentLink = () => {
+  const currentPage = urlParams.get("page");
+  links.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("to") == currentPage) link.classList.add("active");
+  });
+};
+
+if (!urlParams.get("page")) {
+  urlParams.set("page", "balance");
+  window.history.pushState({ page_id: "balance" }, "", `?${urlParams}`);
 }
+handleCurrentLink();
 
 links.forEach((link) => {
   link.addEventListener("click", (e) => {
     e.preventDefault();
-    const state = {}
-    state.page_id = e.target.innerHTML.replace(" ","-");
-    urlParams.set("page",`${e.target.getAttribute("to")}`);
-    window.history.pushState(state,"",`?${urlParams}`)
+    const state = {};
+    state.page_id = e.target.innerHTML.replace(" ", "-");
+    urlParams.set("page", `${e.target.getAttribute("to")}`);
+    window.history.pushState(state, "", `?${urlParams}`);
+    handleCurrentLink();
   });
 });

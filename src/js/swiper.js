035e2dcc4e13b8ae57a235ecbaaa5e2b3babb83c
deigo-lps <diego.lopes0909@gmail.com>
2023-08-15 17:11:11 -0300

    collagen swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -7,19 +7,37 @@ const getPaginationImages = (index, className, images) => {
 const imagesPre = document.querySelectorAll(
   ".swiper-pre-workout .swiper-slide img"
 );
-const swiperPreWorkout = new Swiper(".swiper-pre-workout", {
+
+const baseConfig = {
   slidesPerView: 1,
   spaceBetween: 16,
   loop: true,
   speed: 500,
-  autoplay:{
+  autoplay: {
     delay: 2500,
     disableOnInteraction: true,
   },
+};
+
+const swiperPreWorkout = new Swiper(".swiper-pre-workout", {
+  ...baseConfig,
   pagination: {
     el: ".pre-workout-pagination",
     clickable: true,
     renderBullet: (index, className) =>
       getPaginationImages(index, className, imagesPre),
+  },
+});
+
+const imagesCollagen = document.querySelectorAll(
+  ".swiper-collagen .swiper-slide img"
+);
+const swiperCollagen = new Swiper(".swiper-collagen", {
+  ...baseConfig,
+  pagination: {
+    el: ".collagen-pagination",
+    clickable: true,
+    renderBullet: (index, className) =>
+      getPaginationImages(index, className, imagesCollagen),
   },
 });
